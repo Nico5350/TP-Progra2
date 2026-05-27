1f331e69559d831100a1a844a2e0fbbc0883daa0
Nico5350 <nicocontreras5350@gmail.com>
2026-05-27 12:14:42 -0300

    Cambios en clase cuenta

--- src/main/Cuenta.java
+++ src/main/Cuenta.java
@@ -1,72 +1,66 @@
 package main;
+
 import java.util.ArrayList;
 
-public class Cuenta {
+public abstract class Cuenta {
 
     private String cvu;
     private String alias;
     private double saldo;
-
     private Usuario titular;
-
-    private ArrayList<String> historialMovimientos;
+    private ArrayList<Actividad> actividades;
 
     public Cuenta(String cvu, String alias, Usuario titular) {
-
         this.cvu = cvu;
         this.alias = alias;
         this.titular = titular;
-
-        saldo = 0;
-
-        historialMovimientos = new ArrayList<>();
-    }
-
-    public String getCvu() {
-        return cvu;
-    }
-
-    public String getAlias() {
-        return alias;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public Usuario getTitular() {
-        return titular;
-    }
-
-    public ArrayList<String> getHistorialMovimientos() {
-        return historialMovimientos;
+        this.saldo = 0;
+        this.actividades = new ArrayList<Actividad>();
     }
 
     public void depositar(double monto) {
-
         saldo += monto;
-
-        historialMovimientos.add(
-                "Depósito de $" + monto
-        );
     }
 
     public void retirar(double monto) {
-
         saldo -= monto;
-
-        historialMovimientos.add(
-                "Retiro de $" + monto
-        );
     }
+
+    public void registrarActividad(Actividad a) {
+        actividades.add(a);
+    }
+
+    public ArrayList<Actividad> obtenerActividades() {
+        return actividades;
+    }
+
+    public double obtenerSaldo() {
+        return saldo;
+    }
+
+    public String obtenerCvu() {
+        return cvu;
+    }
+
+    public String obtenerAlias() {
+        return alias;
+    }
+
+    public Usuario obtenerTitular() {
+        return titular;
+    }
+
+    public abstract String obtenerTipo();
 
     @Override
     public String toString() {
-
-        return "Cuenta{" +
-                "cvu='" + cvu + '\'' +
-                ", alias='" + alias + '\'' +
-                ", saldo=" + saldo +
-                '}';
+        StringBuilder sb = new StringBuilder();
+        sb.append(obtenerTipo());
+        sb.append(": ");
+        sb.append(alias);
+        sb.append(" (");
+        sb.append(cvu);
+        sb.append(")");
+        return sb.toString();
     }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module Billetera.ar {
+	requires junit;
 }
