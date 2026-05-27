15a386110ead53d25e213679f46704d8ba4d681d
Nico5350 <nicocontreras5350@gmail.com>
2026-05-27 12:38:37 -0300

    Cambios en cuenta regular y premium para que validen con sus caracteristicas

--- src/main/CuentaPremium.java
+++ src/main/CuentaPremium.java
@@ -1,9 +1,22 @@
 package main;
 
-public class CuentaPremium extends Cuenta //creo un sistema de herencia para las tres tipos de cuentas, siguiendo el formato presentado en el esquema de la primer entrega
-
-    public CuentaRegular(String cvu, String alias, Usuario titular) {
+public class CuentaPremium extends Cuenta{ 
+	private static double depositoMinimo = 500000;
+    public CuentaPremium(String cvu, String alias, Usuario titular,double depositoInicial) {
 
         super(cvu, alias, titular);
+        
+        if (depositoInicial < depositoMinimo) {
+        	throw new IllegalArgumentException("El deposito inicial no puede ser menor a 500000");
+        }
+        depositar(depositoInicial);
+    }
+    public static double obtenerDepositoMinimo(){
+    	return depositoMinimo;
+    }
+    
+    @Override
+    public String obtenerTipo() { //para clase cuenta
+    	return "Premium";
     }
 }
--- src/main/CuentaRegular.java
+++ src/main/CuentaRegular.java
@@ -1,9 +1,17 @@
 package main;
 
-public class CuentaRegular extends Cuenta {         //creo un sistema de herencia para las tres tipos de cuentas, siguiendo el formato presentado en el esquema de la primer entrega
-
-    public CuentaRegular(String cvu, String alias, Usuario titular) {
-
+public class CuentaRegular extends Cuenta{     
+	private static double saldoMaximo = 5000000; // saldo maximo de cuenta regular
+    
+	public CuentaRegular(String cvu, String alias, Usuario titular) {
         super(cvu, alias, titular);
     }
+	public static double obtenerSaldoMaximo() { //para que billetera pueda validar el saldo 
+		return saldoMaximo;
+	}
+	
+	@Override
+	public String obtenerTipo() { //para clase cuenta
+		return "Regular";
+	}
 }
