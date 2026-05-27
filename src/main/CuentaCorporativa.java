63b57949bfea5f45f7390c8dbc1ecdd529771799
Nico5350 <nicocontreras5350@gmail.com>
2026-05-27 13:17:58 -0300

    Clases usuario y cuenta corporativa terminadas

--- src/main/CuentaCorporativa.java
+++ src/main/CuentaCorporativa.java
@@ -1,7 +1,21 @@
 package main;
 
 public class CuentaCorporativa extends Cuenta{
-	public CuentaCorporativa(String cvu, String alias, Usuario titular) {
+	private Empresa empresa;
+	public CuentaCorporativa(String cvu, String alias, Usuario titular, Empresa empresa) {
 		super(cvu,alias,titular);
+		
+		if (!empresa.estaAutorizado(titular.obtenerDni())) {
+		    throw new IllegalArgumentException("El titular no esta autorizado en la empresa");
+		}
+		this.empresa = empresa;
+	}
+	public Empresa obtenerEmpresa() {
+		return empresa;
+	}
+	
+	@Override
+	public String obtenerTipo() {
+		return "Corporativa";
 	}
 }
--- src/main/Usuario.java
+++ src/main/Usuario.java
@@ -1,11 +1,14 @@
 package main;
 
+import java.util.ArrayList;
+
 public class Usuario {
 	private String dni;
 	private String nombre;
 	private String telefono;
 	private String email;
 	private double totalInvertido;
+	private ArrayList<Cuenta> cuentas;
 	
 	public Usuario(String dni, String nombre, String telefono, String email) {
 		super();
@@ -14,8 +17,43 @@ public class Usuario {
 		this.telefono = telefono;
 		this.email = email;
 		this.totalInvertido = 0;
+		this.cuentas = new ArrayList<Cuenta>();
+	}
+	public String obtenerDni() {
+		return dni;
 	}
 	
+	public String obtenerNombre() {
+		return nombre;
+	}
 	
+	public double obtenerTotalInvertido() {
+		return totalInvertido;
+	}
 	
+	public ArrayList<Cuenta> obtenerCuentas(){
+		return cuentas;
+	}
+	
+	public void agregarCuentas(Cuenta cuenta) {
+		cuentas.add(cuenta);
+	}
+	
+	public void agregarInvertido(double monto) {
+		totalInvertido += monto;
+	}
+	
+	public void restarInvertido(double monto) {
+		totalInvertido -= monto;
+	}
+	@Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Usuario: ").append(nombre);
+        sb.append(" | DNI: ").append(dni);
+        sb.append(" | Tel: ").append(telefono);
+        sb.append(" | Email: ").append(email);
+        sb.append(" | Total invertido: $").append(totalInvertido);
+        return sb.toString();
+    }
 }
